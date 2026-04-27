4bdbfa556246f30bad67b64171eabe98fc4ca43d
ortegauros <ortega.nuria@gmail.com>
2026-04-27 17:10:33 +0000

    mi primer commit

---
+++ mi_primer_programa.py
@@ -0,0 +1,2 @@
+#programa que escribe un saludo por pantall dicciendo que curso mola mucho
+print("Hola, el curso de Python mola mucho")
---
+++ mi_segundo_programa.py
@@ -0,0 +1,7 @@
+#programa preguntame el nombre de mi tres mejores amigos y luego imprime un saludo para cada uno
+nombre1 = input("¿Cuál es el nombre de tu primer amigo? ")
+nombre2 = input("¿Cuál es el nombre de tu segundo amigo? ")
+nombre3 = input("¿Cuál es el nombre de tu tercer amigo? ")  
+print("Hola, " + nombre1 + "! Es genial tenerte como amigo.")
+print("Hola, " + nombre2 + "! Es genial tenerte como amigo.")
+print("Hola, " + nombre3 + "! Es genial tenerte como amigo.")
